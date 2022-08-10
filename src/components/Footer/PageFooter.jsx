b002f28c1c3e52f6d6ead302d96171b41362dd3f
leonelborgesdev <leonel03t@gmail.com>
2022-08-10 17:36:08 -0400

    ioicon footer

--- src/components/Footer/PageFooter.jsx
+++ src/components/Footer/PageFooter.jsx
@@ -4,6 +4,7 @@ import logo_instagram from "../../assets/logo-instagram.svg";
 import logo_github from "../../assets/logo-github.svg";
 import logo_linkedin from "../../assets/logo-linkedin.svg";
 import logo_twitter from "../../assets/logo-twitter.svg";
+import { IonIcon } from "@ionic/react";
 
 export const PageFooter = () => {
   return (
@@ -19,22 +20,23 @@ export const PageFooter = () => {
           <ul className="social_icon">
             <li>
               <a href="https://www.linkedin.com/in/leonelborgesdev">
-                <img src={logo_linkedin} width={"30px"} height={"30px"} />
+                <IonIcon src={logo_linkedin} />
               </a>
             </li>
             <li>
               <a href="https://github.com/leonelborgesdev">
-                <img src={logo_github} width={"30px"} height={"30px"} />
+                <IonIcon src={logo_github} />
               </a>
             </li>
             <li>
               <a href="#">
-                <img src={logo_instagram} width={"30px"} height={"30px"} />
+                <IonIcon src={logo_instagram} />
               </a>
             </li>
             <li>
               <a href="#">
-                <img src={logo_twitter} width={"30px"} height={"30px"} />
+                <IonIcon src={logo_twitter} />
+                {/* <img src={logo_twitter} width={"30px"} height={"30px"} /> */}
               </a>
             </li>
           </ul>
