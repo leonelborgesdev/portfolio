c2e483ccbac7ee8697c9024f7ce8f2ee87334973
leonelborgesdev <leonel03t@gmail.com>
2022-08-12 09:47:56 -0400

    traduciendo about me

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -13,6 +13,24 @@ export const changeLanguaje = (languaje) => {
     dispatch({
       type: CHANGE_LANGUAJE,
       payload: languaje,
+      payloadAboutMe: aboutMe(languaje),
     });
   };
+};
+const aboutMe = (languaje) => {
+  if (languaje === "español") {
+    return [
+      "👋🏻Hola😃",
+      "Me llamo Leonel Borges, soy desarrollador web, vivo en la ciudad de Bermejo, de Tarija Bolivia, mis hobies son el voley, la natacion y los deportes. Soy Graduado Full Stack Developer, con aptitudes en Backend y Frontend. Me gusta programar, me gusta aprender mas dia a dia y trato de mejorar mis habilidades estudiando, practicando y participando en proyectos de tecnilogia.",
+      "🔥Fortalezas: El trabajo en equipo se me da muy bien, me adapto facilmente a los cambios, soy participativo, consulto y también doy asesoría a mis compañeros de equipo, si hay algo que no se, lo investigo y me esfuerzo mucho por encontrar la respuesta.",
+      "🧑‍💻💼Experiencia: Por ahora mi experiencia es netamente academica, participando en proyectos individuales y grupales como ser los proyectos en henry y otros, tambien he sido ayudante de catedra en la UAJMS, ayudando a los chicos con sus practicos, tareas y preparandolos para sus examenes de programacion en Java",
+    ];
+  } else {
+    return [
+      "👋🏻Hi!!😃",
+      "My name is Leonel Borges, i am a developer web, im live in the city Bermejo, from Tarija Bolivia, mis hobies son el voley, la natacion y los deportes. Soy Graduado Full Stack Developer, con aptitudes en Backend y Frontend. Me gusta programar, me gusta aprender mas dia a dia y trato de mejorar mis habilidades estudiando, practicando y participando en proyectos de tecnilogia.",
+      "🔥Fortalezas: El trabajo en equipo se me da muy bien, me adapto facilmente a los cambios, soy participativo, consulto y también doy asesoría a mis compañeros de equipo, si hay algo que no se, lo investigo y me esfuerzo mucho por encontrar la respuesta.",
+      "🧑‍💻💼Experiencia: Por ahora mi experiencia es netamente academica, participando en proyectos individuales y grupales como ser los proyectos en henry y otros, tambien he sido ayudante de catedra en la UAJMS, ayudando a los chicos con sus practicos, tareas y preparandolos para sus examenes de programacion en Java",
+    ];
+  }
 };
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -39,7 +39,7 @@ const initialState = {
     },
   ],
   about_me: [
-    "👋🏻Hola😃",
+    "👋🏻Hola !!!😃",
     "Me llamo Leonel Borges, soy de la desarrollador web, vivo en la ciudad de Bermejo, de Tarija Bolivia, mis hobies son el voley, la natacion y los deportes. Soy Graduado Full Stack Developer, con aptitudes en Backend y Frontend. Me gusta programar, me gusta aprender mas dia a dia y trato de mejorar mis habilidades estudiando, practicando y participando en proyectos de tecnilogia.",
     "🔥Fortalezas: El trabajo en equipo se me da muy bien, me adapto facilmente a los cambios, soy participativo, consulto y también doy asesoría a mis compañeros de equipo, si hay algo que no se, lo investigo y me esfuerzo mucho por encontrar la respuesta.",
     "🧑‍💻💼Experiencia: Por ahora mi experiencia es netamente academica, participando en proyectos individuales y grupales como ser los proyectos en henry y otros, tambien he sido ayudante de catedra en la UAJMS, ayudando a los chicos con sus practicos, tareas y preparandolos para sus examenes de programacion en Java",
@@ -60,6 +60,7 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         languaje: action.payload,
+        about_me: action.payloadAboutMe,
       };
     }
     default:
