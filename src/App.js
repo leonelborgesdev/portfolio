112ff68c74f9320a25657b32bda47002417d6afd
leonelborgesdev <leonel03t@gmail.com>
2022-08-13 23:06:18 -0400

    fondo obscuro

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Portfolio } from "./components/Portfolio/Portfolio";
+import { FondoOscuro } from "./components/FondoOscuro/FondoOscuro";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Portfolio />} />
+        <Route exact path="/" element={<FondoOscuro />} />
       </Routes>
     </div>
   );
---
+++ src/components/FondoOscuro/FondoOscuro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FondoOscuro.css";
+
+export const FondoOscuro = () => {
+  return (
+    <div className="container_dark_all">
+      <div className="container_body">
+        <div className="container_dark">
+          <div className="bubbles"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
