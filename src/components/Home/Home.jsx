29aa36ff63fe20c3eede27a54136c128a6413bc1
leonelborgesdev <leonel03t@gmail.com>
2022-08-15 09:39:51 -0400

    inicion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,54 +20,54 @@ export const Home = () => {
   const { cards, about_me, separadores_portfolio, mode } = useSelector(
     (state) => state
   );
-  gsap.registerPlugin(ScrollTrigger);
-  React.useEffect(() => {
-    gsap.to("#bg", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      scale: 1.5,
-    });
-    gsap.to("#man", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      scale: 0.5,
-    });
-    gsap.to("#clouds_1", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      x: 200,
-    });
-    gsap.to("#clouds_2", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      x: -200,
-    });
-    gsap.to("#mountain_left", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      x: -500,
-    });
-    gsap.to("#mountain_rigth", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      x: 500,
-    });
-    gsap.to("#text", {
-      scrollTrigger: {
-        scrub: 1,
-      },
-      x: 500,
-    });
-  });
+  // gsap.registerPlugin(ScrollTrigger);
+  // React.useEffect(() => {
+  //   gsap.to("#bg", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     scale: 1.5,
+  //   });
+  //   gsap.to("#man", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     scale: 0.5,
+  //   });
+  //   gsap.to("#clouds_1", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     x: 200,
+  //   });
+  //   gsap.to("#clouds_2", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     x: -200,
+  //   });
+  //   gsap.to("#mountain_left", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     x: -500,
+  //   });
+  //   gsap.to("#mountain_rigth", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     x: 500,
+  //   });
+  //   gsap.to("#text", {
+  //     scrollTrigger: {
+  //       scrub: 1,
+  //     },
+  //     x: 500,
+  //   });
+  // });
   return (
     <div id="inicio" className="container_home">
-      <section className="container_section">
+      {/* <section className="container_section">
         {mode === "light" ? (
           <>
             <img src={bg} id="bg" />
@@ -81,7 +81,9 @@ export const Home = () => {
         ) : (
           <FondoOscuro />
         )}
-      </section>
+      </section> */}
+
+      <FondoOscuro />
       <div id="about" className="sec">
         <h2>Portfolio</h2>
         <div className="container_about_me">
