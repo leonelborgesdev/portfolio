a205891523783803d6cb98bfbbab391dc1ae440b
leonelborgesdev <leonel03t@gmail.com>
2022-08-16 18:12:16 -0400

    button play

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Portfolio } from "./components/Portfolio/Portfolio";
+import { BtnVideo } from "./components/Video/BtnVideo";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Portfolio />} />
+        <Route exact path="/" element={<BtnVideo />} />
       </Routes>
     </div>
   );
---
+++ src/components/Video/BtnVideo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BtnVideo.css";
+
+export const BtnVideo = () => {
+  return (
+    <div className="container_all_video">
+      <div className="container_body_video">
+        <div className="btn_play">
+          <div className="play"></div>
+          <p>Play Video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
