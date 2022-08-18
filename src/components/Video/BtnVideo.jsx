a3e40e92c36bbc6e414376521b27f0e2292f1f7a
leonelborgesdev <leonel03t@gmail.com>
2022-08-17 23:59:14 -0400

    btn play video

--- src/components/Video/BtnVideo.jsx
+++ src/components/Video/BtnVideo.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useRef } from "react";
+import ReactPlayer from "react-player";
 import "./BtnVideo.css";
 
 export const BtnVideo = () => {
+  const btnPlayRef = useRef();
+  const handleAddClass = () => {
+    const btnPlayRefNode = btnPlayRef.current;
+    btnPlayRefNode.classList.add("active");
+  };
   return (
     <div className="container_all_video">
       <div className="container_body_video">
-        <div className="btn_play">
+        <div className="btn_play" ref={btnPlayRef} onClick={handleAddClass}>
           <div className="play"></div>
           <p>Play Video</p>
+        </div>
+        <div className="clip">
+          <iframe
+            width="853"
+            height="480"
+            src="https://www.youtube.com/embed/6ZD9OhFFKkk"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+          <b>Close</b>
         </div>
       </div>
     </div>
