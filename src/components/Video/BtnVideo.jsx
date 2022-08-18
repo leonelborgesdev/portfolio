5123f9513871d439028f7dd164fd8a21f4d02f78
leonelborgesdev <leonel03t@gmail.com>
2022-08-18 00:27:59 -0400

    play video

--- src/components/Video/BtnVideo.jsx
+++ src/components/Video/BtnVideo.jsx
@@ -5,9 +5,20 @@ import "./BtnVideo.css";
 
 export const BtnVideo = () => {
   const btnPlayRef = useRef();
+  const clipRef = useRef();
+  const closeRef = useRef();
   const handleAddClass = () => {
     const btnPlayRefNode = btnPlayRef.current;
+    const clipRefNode = clipRef.current;
     btnPlayRefNode.classList.add("active");
+    clipRefNode.classList.add("active");
+  };
+  const handleRemove = () => {
+    const closeRefNode = closeRef.current;
+    const btnPlayRefNode = btnPlayRef.current;
+    const clipRefNode = clipRef.current;
+    btnPlayRefNode.classList.remove("active");
+    clipRefNode.classList.remove("active");
   };
   return (
     <div className="container_all_video">
@@ -16,14 +27,16 @@ export const BtnVideo = () => {
           <div className="play"></div>
           <p>Play Video</p>
         </div>
-        <div className="clip">
+        <div className="clip" ref={clipRef}>
           <iframe
             width="853"
             height="480"
             src="https://www.youtube.com/embed/6ZD9OhFFKkk"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
-          <b>Close</b>
+          <b className="close" ref={closeRef} onClick={handleRemove}>
+            Close
+          </b>
         </div>
       </div>
     </div>
