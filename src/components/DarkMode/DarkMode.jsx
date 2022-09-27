b192f4a4c5b7940d2207b4c369061fe5ada00e46
leonelborgesdev <leonel03t@gmail.com>
2022-09-26 23:22:46 -0400

    dark mode

---
+++ src/components/DarkMode/DarkMode.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./DarkMode.css";
+
+export const DarkMode = () => {
+  return (
+    <div className="containerBodyDarkMode">
+      <section>
+        <div className="layer layer1"></div>
+        <div className="layer layer2"></div>
+        <div className="layer layer3"></div>
+        <div className="layer layer4"></div>
+        <div className="layer layer5"></div>
+        <div className="layer layer6"></div>
+        <div className="layer layer7"></div>
+        <div className="layer layer8"></div>
+        <div className="layer layer9"></div>
+      </section>
+    </div>
+  );
+};
