fa5c4cdf5c4b93c2761e2f686f2e025ba10bd13a
leonelborgesdev <leonel03t@gmail.com>
2022-09-27 00:25:40 -0400

    portfolio

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Portfolio } from "./components/Portfolio/Portfolio";
 import { BtnVideo } from "./components/Video/BtnVideo";
-import { DarkMode } from "./components/DarkMode/DarkMode";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<DarkMode />} />
+        <Route exact path="/" element={<Portfolio />} />
         <Route exact path="/:id" element={<BtnVideo />} />
       </Routes>
     </div>
--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 import "./DarkMode.css";
+import Typed from "typed.js";
+
+const TypedReactHooksDemo = () => {
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
+
+  React.useEffect(() => {
+    const options = {
+      strings: [
+        `<h3><span class="word_const">const</span><span class="word_Profile"> Profile</span>=<span class="word_llave">{</span></h3><h3><span class="word_attrib"> Name: </span><span class="word_string">"Leonel Borges"</span><span class="word_Profile">,</span></h3><h3><span class="word_attrib"> Profesion: </span><span class="word_string">"Full Stack Developer"</span><span class="word_Profile">,</span></h3><h3><span class="word_llave">}</span></h3>`,
+      ],
+      typeSpeed: 30,
+    };
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+  }, []);
+
+  return (
+    <div className="type-wrap">
+      <span ref={el} />
+    </div>
+  );
+};
+document.getElementById("react-root");
 
 export const DarkMode = () => {
   return (
@@ -14,6 +40,9 @@ export const DarkMode = () => {
         <div className="layer layer7"></div>
         <div className="layer layer8"></div>
         <div className="layer layer9"></div>
+        <div className="Title_Portfolio">
+          <TypedReactHooksDemo />
+        </div>
       </section>
     </div>
   );
