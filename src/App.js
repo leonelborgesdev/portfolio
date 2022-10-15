4756219c7eb3bd8e283d4124d17eadc0f2871657
leonelborgesdev <leonel03t@gmail.com>
2022-10-15 00:57:42 -0400

    portfolio

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import { Portfolio } from "./components/Portfolio/Portfolio";
 import { BtnVideo } from "./components/Video/BtnVideo";
 import { Home } from "./components/Home/Home";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { language } = useSelector((state) => state);
   return (
     <div className="App">
       <Routes>
