1f6391a7819a931a0960775354968cf43b0dffa3
leonelborgesdev <leonel03t@gmail.com>
2024-01-22 22:13:06 -0400

    quitando comentarios

--- src/components/Video/BtnVideo.jsx
+++ src/components/Video/BtnVideo.jsx
@@ -3,16 +3,6 @@ import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import breeds2 from "../../assets/breeds2.png";
-import BarberApp2 from "../../assets/BarberApp2.png";
-// import BarberApp3 from "../../assets/BarberApp3.png";
-// import BarberApp4 from "../../assets/BarberApp4.png";
-// import BarberApp5 from "../../assets/BarberApp5.png";
-// import BarberApp6 from "../../assets/BarberApp6.png";
-// import BarberApp7 from "../../assets/BarberApp7.png";
-import Countries2 from "../../assets/Countries2.png"; //Countries
-import marketplace2 from "../../assets/marketplace2.png";
-import pokemons from "../../assets/pokemons.png";
 import "./BtnVideo.css";
 import { Carrusel } from "../Carrusel/Carrusel";
 
@@ -23,12 +13,6 @@ export const BtnVideo = () => {
   const btnPlayRef = useRef();
   const clipRef = useRef();
   const closeRef = useRef();
-  // const handleAddClass = () => {
-  //   const btnPlayRefNode = btnPlayRef.current;
-  //   const clipRefNode = clipRef.current;
-  //   btnPlayRefNode.classList.add("active");
-  //   clipRefNode.classList.add("active");
-  // };
   const handleRemove = () => {
     const btnPlayRefNode = btnPlayRef.current;
     const clipRefNode = clipRef.current;
@@ -66,21 +50,11 @@ export const BtnVideo = () => {
             )}
           </div>
         </div>
-        {/* <div className="btn_play" ref={btnPlayRef} onClick={handleAddClass}>
-          <div className="play"></div>
-          <p>Play Video</p>
-        </div> */}
         <Link to={`/`} className="buton_back">
           {language === "español" ? "Volver" : "Back"}
         </Link>
         <div className="clip" ref={clipRef}>
           <div>Video {card[0].title}</div>
-          {/* <iframe
-            width="853"
-            height="480"
-            src="https://www.youtube.com/embed/6ZD9OhFFKkk"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe> */}
           <b className="close" ref={closeRef} onClick={handleRemove}>
             {language === "español" ? "Cerrar" : "Close"}
           </b>
