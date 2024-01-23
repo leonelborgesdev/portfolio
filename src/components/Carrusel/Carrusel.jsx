568ce8c96fd4c93794bb95ce4f22bad2bd0c4c54
leonelborgesdev <leonel03t@gmail.com>
2024-01-22 21:38:31 -0400

    carrusel

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -6,5 +8,32 @@ export const Carrusel = () => {
   const { cards } = useSelector((state) => state);
   const { id } = useParams();
   const card = cards.filter((card) => card.id === id);
-  return <div>{card[0].title}</div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    "https://github.com/leonelborgesdev/leonelborgesdev/assets/95236206/173a4733-ed7c-424b-aede-6c87b054d624",
+    "https://github.com/leonelborgesdev/leonelborgesdev/assets/95236206/a2b0d113-4e62-4db0-aa96-13487e8c8bd2",
+    "https://github.com/leonelborgesdev/leonelborgesdev/assets/95236206/340759a6-66a8-4866-b425-264b6c37fe23",
+    // Agrega más imágenes según sea necesario
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Cambia el tiempo de transición según tus necesidades
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, images.length]);
+  return (
+    <div className="carousel-container">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`carousel-slide ${index === currentIndex ? "active" : ""}`}
+        >
+          <img src={image} alt={`Imagen ${index + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
 };
